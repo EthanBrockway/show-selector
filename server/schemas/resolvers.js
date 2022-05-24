f59f77510535eed408f3743663c768f128cd3fc4
Skronkie <ebrockway01@gmail.com>
2022-05-24 18:54:46 -0500

    added server.js essentials, created connect.js folder, added a user model and a few test typedefs/resolvers

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,18 @@
+const { User } = require("../models");
+const { signToken } = require("../utils/auth");
+const resolvers = {
+  Query: {
+    users: async () => {
+      return User.find().select(`-__v, -password`);
+    },
+  },
+  Mutation: {
+    AddUser: async (parent, args) => {
+      const user = await User.create(args);
+
+      return { user };
+    },
+  },
+};
+
+module.exports = resolvers;
